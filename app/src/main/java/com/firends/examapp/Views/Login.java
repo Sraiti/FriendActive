91fabe99ed8449f3530a0405ec93e1825357ebfa
khalidabdou <37934938+khalidabdou@users.noreply.github.com>
2019-12-06 11:15:25 +0100

    ads manager

--- app/src/main/java/com/firends/examapp/Views/Login.java
+++ app/src/main/java/com/firends/examapp/Views/Login.java
@@ -127,6 +127,8 @@ public class Login extends AppCompatActivity {
                 } else {
 
                     NewUser.set_UserName(user.getDisplayName());
+                    editor.putString("name", user.getDisplayName());
+                    editor.commit();
                     User.currentUser = NewUser;
                     manager.AddUser(NewUser, FirebaseInstanceId.getInstance().getToken());
                     startAvtivity();
